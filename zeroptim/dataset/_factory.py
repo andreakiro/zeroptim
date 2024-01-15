0445b03efe6a0aa7243554d5172ec46cac55c754
Andrea Pinto <andreakiro.pinto@gmail.com>
2024-01-15 17:56:46 -0500

    bugfix

--- zeroptim/dataset/_factory.py
+++ zeroptim/dataset/_factory.py
@@ -10,5 +10,5 @@ class DataLoaderFactory:
     @staticmethod
     def init_loader(config: Config) -> DataLoader:
         dataset_name: str = config.dataset.name
-        dataset_params: Dict[str, Any] = config.dataset.dataset_params or {}
+        dataset_params: Dict[str, Any] = config.dataset.params or {}
         return VisionDataset.make_loader(dataset_name, **dataset_params)
