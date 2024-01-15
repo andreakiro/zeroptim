c507fc1af13c04df635ed4e5d1255db99282e874
Andrea Pinto <andreakiro.pinto@gmail.com>
2024-01-15 14:37:19 -0500

    add dataset sampler

--- zeroptim/dataset/utils.py
+++ zeroptim/dataset/utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import random
 import torch
+
 
 def visualize_samples(dataloader: torch.utils.data.DataLoader, num_samples: int = 5):
     """
@@ -25,5 +27,27 @@ def visualize_samples(dataloader: torch.utils.data.DataLoader, num_samples: int 
         img = img.clip(0, 1)  # Clip to ensure it's a valid image
         ax.imshow(img)
         ax.set_title(f"Label: {labels[i].item()}")
-        ax.axis('off')
+        ax.axis("off")
     plt.show()
+
+
+def sample(loader, num_batches=1):
+    dataset = loader.dataset
+    batch_size = loader.batch_size
+    collate_fn = loader.collate_fn
+    total_batches = len(loader)
+
+    sampled_indices = random.sample(range(total_batches), num_batches)
+
+    for batch_idx in sampled_indices:
+        start_idx = batch_idx * batch_size
+        end_idx = min(start_idx + batch_size, len(dataset))
+        batch_samples = [dataset[i] for i in range(start_idx, end_idx)]
+
+        if collate_fn:
+            batch = collate_fn(batch_samples)
+        else:
+            inputs, targets = zip(*batch_samples)
+            batch = (torch.stack(inputs), torch.stack(targets))
+
+        yield batch
